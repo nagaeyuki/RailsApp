8d94b7b7b0cd2aaf27492d56af41b4afe47d88d2
nagaeyuki <yuki.0123.n@gmail.com>
2017-06-14 14:19:44 +0900

    acts-as-taggable-onを使ってPeopleにタグを追加

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,14 @@
 class Person < ApplicationRecord
+  acts_as_ordered_taggable_on :skills, :interests
+acts_as_taggable # acts_as_taggable_on :tags のエイリアス
+
+# @person.tag_list = "awesome, slick, hefty" # this should be familiar
+# @person.save
+# @person.tags # => [<Tag name:"awesome">,<Tag name:"slick">,<Tag name:"hefty">]
+# #@person.tags_count # => [<Tag name:"awesome" count=2>,<Tag name:"slick">,<Tag name:"hefty" count=2>]
+
   has_many :message
   validates :name, presence: {message: 'は必須項目です'}
   validates :age, numericality: {message: 'は数字で入力してください'}
-  validates :mail, email: {message: 'はメールアドレスではありません'}
+  # validates :mail, email: {message: 'はメールアドレスではありません'}
 end
